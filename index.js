9349415472be2a3aeb5fb62b42df182b7146edbf
Harshita295491 <harshitha.pathakamuri@thebluespire.com>
2024-11-14 10:47:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express= require('express');
+const app= express();
+app.use(express.json())
+
+let items=[
+    {
+       "id":101,
+       "producttype":"oil",
+       "productname":"livon"
+    },
+    {
+        "id":102,
+        "producttype":"cream",
+        "productname":"emolene"
+     },
+     {
+        "id":103,
+        "producttype":"serum",
+        "productname":"niacinsmide"
+     },
+     {
+        "id":104,
+        "producttype":"powder",
+        "productname":"compact"
+     }
+]
+
+
+app.get('/items', (req, res)=>{
+    req.send(items);
+
+});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, (err)=>{
+    if(!err){
+        console.log("server running on port 3000")
+
+    }
+    else{
+        console.log("failed")
+    }
+})
